002776620ea33b8f768d3e09bfa4845d89a2757e
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2020-02-07 07:42:10 +0100

    Initial Commit

---
+++ Ahedfi.Component.Hosting.Domain.Interface/IModule.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+
+namespace Ahedfi.Component.Hosting.Domain.Interface
+{
+    public interface IModule
+    {
+        int Order { get; }
+
+        void RegisterTypes(ContainerBuilder builder);
+    }
+}
---
+++ Ahedfi.Component.Hosting.Domain.Service/AutofacModule.cs
@@ -0,0 +1,11 @@
+﻿using Ahedfi.Component.Hosting.Domain.Interface;
+using Autofac;
+
+namespace Ahedfi.Component.Hosting.Domain.Service
+{
+    public abstract class AutofacModule : Module, IModule
+    {
+        public int Order { get; }
+        public abstract void RegisterTypes(ContainerBuilder builder);
+    }
+}
