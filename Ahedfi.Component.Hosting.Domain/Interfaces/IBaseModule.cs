5d69b530f638cd96f811100b5161bde793cfe8bb
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2020-02-11 21:55:21 +0100

    Refactor Hosting module : Remove autofac and use default dependency injection engine

---
+++ Ahedfi.Component.Hosting.Domain/Interfaces/IBaseModule.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ahedfi.Component.Hosting.Domain.Interfaces
+{
+    public interface IBaseModule
+    {
+        int Order { get; }
+
+        void RegisterTypes(IServiceCollection container);
+    }
+}
---
+++ Ahedfi.Component.Hosting.Domain/Services/BaseModule.cs
@@ -0,0 +1,13 @@
+﻿using Ahedfi.Component.Hosting.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ahedfi.Component.Hosting.Domain.Services
+{
+    public abstract class BaseModule : IBaseModule
+    {
+        public virtual int Order { get; }
+        public virtual void RegisterTypes(IServiceCollection container)
+        {
+        }
+    }
+}
