2fc04a4d1aeb745731594c1bab323790cbd75bcd
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2020-02-19 20:42:51 +0100

    Add IBusiness

---
+++ Ahedfi.Component.Services.Domain/Inerfaces/IBusiness.cs
@@ -0,0 +1,17 @@
+﻿using Ahedfi.Component.Core.Domain.Models.Interfaces;
+using Ahedfi.Component.Core.Domain.Security.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Ahedfi.Component.Services.Domain.Inerfaces
+{
+    public interface IBusiness<T> where T : IEntity
+    {
+        T Save(IUserIdentity user, T Entity);
+        bool Delete(IUserIdentity user, T Entity);
+        T FindFirstOrDefault(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> Filter(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> FindAll();
+    }
+}
