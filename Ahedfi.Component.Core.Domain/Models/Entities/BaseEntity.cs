6be29cf12817d6406b7eddbab9d63d3b3f717743
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2020-02-19 20:29:39 +0100

    Add ITransient Inteface

--- Ahedfi.Component.Core.Domain/Models/Entities/BaseEntity.cs
+++ Ahedfi.Component.Core.Domain/Models/Entities/BaseEntity.cs
@@ -2,9 +2,10 @@
 
 namespace Ahedfi.Component.Core.Domain.Models.Entities
 {
-    public class BaseEntity<Tkey> : Entity, IEntity<Tkey>, IDeleted where Tkey : struct
+    public class BaseEntity<Tkey> : Entity, IEntity<Tkey>, IDeleted, ITransient where Tkey : struct
     {
         public Tkey Id { get; set; }
         public bool IsDeleted { get; set; }
+        public bool IsTransient => Id.Equals(default(Tkey));
     }
 }
---
+++ Ahedfi.Component.Core.Domain/Models/Interfaces/ITransient.cs
@@ -0,0 +1,7 @@
+﻿namespace Ahedfi.Component.Core.Domain.Models.Interfaces
+{
+    public interface ITransient
+    {
+        bool IsTransient { get; }
+    }
+}
