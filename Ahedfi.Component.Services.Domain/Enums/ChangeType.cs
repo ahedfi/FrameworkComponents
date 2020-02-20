98374ffdafc5ea7c2c60a3a2a6abb99d35044d11
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2020-02-20 21:36:15 +0100

    Add Enum ChangeType

---
+++ Ahedfi.Component.Services.Domain/Enums/ChangeType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ahedfi.Component.Services.Domain.Enums
+{
+    public enum ChangeType
+    {
+        Add = 1,
+        Update,
+        Delete
+    }
+}
